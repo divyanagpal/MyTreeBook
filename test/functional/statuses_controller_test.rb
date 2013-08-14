1e0b2149e9f538c4fb0f50934c3d6f3556935886
Divya Nagpal <divyar2007@gmail.com>
2013-08-14 09:38:13 -0400

    created avatars using gravatar

--- test/functional/statuses_controller_test.rb
+++ test/functional/statuses_controller_test.rb
@@ -11,14 +11,30 @@ class StatusesControllerTest < ActionController::TestCase
     assert_not_nil assigns(:statuses)
   end
 
-  test "should get new" do
+  test "should be redirected to when not logged in" do
     get :new
-    assert_response :success
+    assert_response :redirect
+    assert_redirected_to new_user_session_path
   end
 
-  test "should create status" do
+
+  test "should render the new page when logged in" do
+    sign_in users(:divya)
+    get :new
+    assert_response :success
+    
+  end
+
+test  "should be logged in to post status" do
+  post :create, status: {content: "Hello"}
+  assert_response :redirect
+  assert_redirected_to new_user_session_path
+end
+
+  test "should create status when logged in" do
+    sign_in users(:divya)
     assert_difference('Status.count') do
-      post :create, status: { content: @status.content, name: @status.name }
+      post :create, status: { content: @status.content}
     end
 
     assert_redirected_to status_path(assigns(:status))
@@ -35,7 +51,7 @@ class StatusesControllerTest < ActionController::TestCase
   end
 
   test "should update status" do
-    put :update, id: @status, status: { content: @status.content, name: @status.name }
+    put :update, id: @status, status: { content: @status.content }
     assert_redirected_to status_path(assigns(:status))
   end
 
--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -29,10 +29,19 @@ user.profile_name = users(:divya).profile_name
   end
 
   test "a user should have a profile name without spaces" do
-   user = User.new
+   user = User.new(first_name: 'divya', last_name: 'nagpal', email: 'divyar2008@gmail.com' )
+    user.password = user.password_confirmation = 'crappyyyy'
    user.profile_name = "My profile with spaces"
    assert !user.save
   	assert !user.errors[:profile_name].empty?
   	assert user.errors[:profile_name].include?("Must be formatted correctly.")
   end
+
+  test "a user can have a correctly formatted profile name" do
+    user = User.new(first_name: 'divya', last_name: 'nagpal', email: 'divyar2008@gmail.com' )
+    user.password = user.password_confirmation = 'crappyyyy'
+    user.profile_name = 'divyanagpal_1'
+    assert user.valid?
+  end
+
 end
